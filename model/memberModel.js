9f3467d4c909d8005aca8261c8914b5dfe4452b6
mingjuan07.24@ichat.sp.edu.sg <mingjuan07.24@ichat.sp.edu.sg>
2026-01-20 14:01:18 +0800

    change_passwordFix

--- model/memberModel.js
+++ model/memberModel.js
@@ -496,52 +496,34 @@ var memberDB = {
             });
         });
     },
-    verifyPassword: function (id, password) {
-        return new Promise((resolve, reject) => {
-            var conn = db.getConnection();
-            conn.connect(function (err) {
+    verifyPassword: function (email, password) {
+    return new Promise((resolve, reject) => {
+        var conn = db.getConnection();
+        conn.connect(err => {
+            if (err) return reject(err);
+
+            const sql = 'SELECT PASSWORDHASH FROM memberentity WHERE EMAIL=?';
+            conn.query(sql, [email], (err, result) => {
                 if (err) {
-                    console.log(err);
                     conn.end();
                     return reject(err);
                 }
-                else {
-                    var sql = 'SELECT * FROM memberentity m WHERE m.ID=?';
-                    conn.query(sql, [id], (err, result) => {
-                        if (err) {
-                            conn.end();
-                            return reject(err);
-                        }
-                        else {
-                            if (result == null || result == undefined || result == '') {
-                                conn.end();
-                                return resolve({ success: false });
-                            }
-                            var member = new Member();
-                            member.email = result[0].EMAIL;
-                            member.passwordHash = result[0].PASSWORDHASH;
 
-                            bcrypt.compare(password, member.passwordHash, function (err, res) {
-                                if (res) {
-                                    console.log("Plain:", password);
-                                    console.log("Hash:", member.passwordHash);
-
-                                    conn.end();
-                                    return resolve({ success: true });
-                                } else {
-                                    console.log("Plain:", password);
-                                    console.log("Hash:", member.passwordHash);
-
-                                    conn.end();
-                                    return resolve({ success: false });
-                                }
-                            });
-                        }
-                    });
+                if (!result || result.length === 0) {
+                    conn.end();
+                    return resolve(false);
                 }
+
+                bcrypt.compare(password, result[0].PASSWORDHASH, (err, match) => {
+                    conn.end();
+                    if (err) return reject(err);
+                    resolve(match);
+                });
             });
         });
-    },
+    });
+}
+,
     updateMemberStripeCustomerId: function (email, customerId) {
         return new Promise((resolve, reject) => {
             var conn = db.getConnection();
