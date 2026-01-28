4df16f02d218fbbc71c8aca87cd96f6646c4e081
mingjuan07.24@ichat.sp.edu.sg <mingjuan07.24@ichat.sp.edu.sg>
2026-01-28 22:38:28 +0800

    fixed_databse_query

--- model/salesrecordModel.js
+++ model/salesrecordModel.js
@@ -1,8 +1,8 @@
 var db = require('./databaseConfig.js');
-
+var SalesRecord = require('./salesRecord.js')
 var salesRecordDB = {
     insertSalesRecord: function (data) {
-        return new Promise((resolve, reject) => {
+        return new Promise( ( resolve, reject ) => {
             var conn = db.getConnection();
             conn.connect(function (err) {
                 if (err) {
@@ -10,60 +10,23 @@ var salesRecordDB = {
                     conn.end();
                     return reject(err);
                 }
-
-                /*
-                  data should contain:
-                  - subtotal
-                  - price (final price after discount)
-                  - promotionId (nullable)
-                  - promotionDiscount
-                  - memberId
-                */
-
-                var sql = `
-                    INSERT INTO salesrecordentity
-                    (
-                        AMOUNTDUE,
-                        AMOUNTPAID,
-                        PROMOTION_DISCOUNT_AMOUNT,
-                        APPLIED_PROMOTION_ID,
-                        CREATEDDATE,
-                        CURRENCY,
-                        MEMBER_ID
-                    )
-                    VALUES (?, ?, ?, ?, ?, ?, ?)
-                `;
-
-                var sqlArgs = [
-                    data.subtotal,                 // original total
-                    data.price,                    // final payable amount
-                    data.promotionDiscount || 0,   // discount amount
-                    data.promotionId || null,      // promotion used
-                    new Date(),
-                    'SGD',
-                    data.memberId
-                ];
-
-                conn.query(sql, sqlArgs, function (err, result) {
-                    conn.end();
-
-                    if (err) {
-                        console.log(err);
-                        return reject(err);
-                    }
-
-                    if (result.affectedRows > 0) {
-                        return resolve({
-                            success: true,
-                            generatedId: result.insertId
-                        });
-                    }
-
-                    return resolve({ success: false });
-                });
+                else {
+                    var sqlArgs = [data.price, data.price, new Date(), 'SGD', data.memberId];
+                    var sql = 'INSERT INTO salesrecordentity (AMOUNTDUE,AMOUNTPAID,CREATEDDATE,CURRENCY,MEMBER_ID) VALUES (?,?,?,?,?)';
+                    conn.query(sql, sqlArgs, function (err, result) {
+                        if (err) {
+                            conn.end();
+                            return reject(err);
+                        } else {
+                            if(result.affectedRows > 0) {
+                                conn.end();
+                                return resolve({success: true, generatedId: result.insertId});
+                            }
+                        }
+                    });
+                }
             });
         });
     }
 };
-
-module.exports = salesRecordDB;
+module.exports = salesRecordDB
